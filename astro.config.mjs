4ef7ecf0faf9023f771107efa51d29145547ea34
BMR <bmr@gmail.com>
2026-08-19 23:49:14 +0530

    feat: added robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
 import node from '@astrojs/node';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://freebulkimagedownloader.com',
+  integrations: [sitemap()],
+  redirects: {
+    '/sitemap.xml': '/sitemap-index.xml',
+  },
   vite: {
     plugins: [tailwindcss()],
   },
